5c051c0bcb7821adda96a608c7b4fecb28727146
Petr Dlouhý <petr.dlouhy@email.cz>
2019-08-06 16:14:42 +0200

    fix config for Django 2.2

--- demoproject/demoproject/settings.py
+++ demoproject/demoproject/settings.py
@@ -108,6 +108,7 @@ TEMPLATES = [{
         ],
         'context_processors': [
             "django.contrib.auth.context_processors.auth",
+            "django.contrib.messages.context_processors.messages",
             "django.template.context_processors.debug",
             "django.template.context_processors.i18n",
             "django.template.context_processors.media",
