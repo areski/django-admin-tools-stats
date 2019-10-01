070e2c854d1cbc5d7c6e019329ac8aca28dc06aa
Petr Dlouhý <petr.dlouhy@email.cz>
2019-10-01 11:56:51 +0200

    all chart parameters are dynamically configurable through Ajax calls

--- admin_tools_stats/urls.py
+++ admin_tools_stats/urls.py
@@ -1,11 +1,11 @@
 
 from django.urls import path
 
-from .views import ChartDataView
+from .views import AdminChartsView, ChartDataView
 
 
 urlpatterns = [
+    path('admin_charts.js', AdminChartsView.as_view(content_type='application/javascript'), name='admin-charts'),
     path('chart_data/', ChartDataView.as_view(), name='chart-data'),  #Only to get the base address in template, will need parameters
-    path('chart_data/<str:interval>/<str:graph_key>/', ChartDataView.as_view(), name='chart-data'),
-    path('chart_data/<str:interval>/<str:graph_key>/<str:select_box_value>/', ChartDataView.as_view(), name='chart-data'),
+    path('chart_data/<str:graph_key>/', ChartDataView.as_view(), name='chart-data'),
 ]
--- admin_tools_stats/views.py
+++ admin_tools_stats/views.py
@@ -1,6 +1,15 @@
+import pytz
+from datetime import datetime
+
 from django.conf import settings
+from django.utils import timezone
 from django.utils.module_loading import import_string
 from django.views.generic import TemplateView
+from django.utils.timezone import now
+
+
+class AdminChartsView(TemplateView):
+    template_name = 'admin_tools_stats/admin_charts.js'
 
 
 class ChartDataView(TemplateView):
@@ -8,15 +17,21 @@ class ChartDataView(TemplateView):
 
     cache_cache_name = "pages"
 
-    def get_context_data(self, *args, interval=None, graph_key=None, select_box_value='', **kwargs):
+    def get_context_data(self, *args, interval=None, graph_key=None, **kwargs):
         context = super().get_context_data(*args, **kwargs)
+        interval = self.request.GET.get('select_box_interval', interval)
+        time_since = datetime.strptime(self.request.GET.get('time_since', None), '%Y-%m-%d')
+        time_since = pytz.utc.localize(time_since)
+        time_until = datetime.strptime(self.request.GET.get('time_until', None), '%Y-%m-%d')
+        time_until = pytz.utc.localize(time_until)
         DashboardChart = import_string(getattr(settings, 'ADMIN_TOOLS_STATS_CHART_APP', 'admin_tools_stats.modules.DashboardChart'))
         module = DashboardChart(
             'today'.title(),
             interval=interval,
+            time_since=time_since,
+            time_until=time_until,
             require_chart_jscss=False,
             graph_key=graph_key,
-            **{"select_box_" + graph_key: select_box_value},
         )
         module.init_with_context_ajax({'request': self.request})
         context['module'] = module
