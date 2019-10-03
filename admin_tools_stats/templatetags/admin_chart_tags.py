cd6fd7528a92f9464a94d1977f332b8fca9172c0
Petr Dlouhý <petr.dlouhy@email.cz>
2019-10-03 16:58:44 +0200

    fix: don't show charts twice in admin_tools, remove unnecessary dashboard.html

--- admin_tools_stats/templatetags/admin_chart_tags.py
+++ admin_tools_stats/templatetags/admin_chart_tags.py
@@ -1,6 +1,7 @@
 
 from django import template
 from django.template.loader import render_to_string
+from django.conf import settings
 
 from ..modules import get_active_graph
 
@@ -10,7 +11,6 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def chart_containers(context):
-    import pprint
-    pprint.pprint(get_active_graph())
     context['charts'] = get_active_graph()
-    return render_to_string("admin_tools_stats/chart_containers.html", context.flatten())
+    if 'admin_tools.dashboard' not in settings.INSTALLED_APPS:
+        return render_to_string("admin_tools_stats/chart_containers.html", context.flatten())
