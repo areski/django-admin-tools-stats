fc3b5b6ca6fc003224387b9db41ce3d128a09807
Petr Dlouhý <petr.dlouhy@email.cz>
2020-01-30 12:32:37 +0100

    add more model tests

--- admin_tools_stats/tests.py
+++ admin_tools_stats/tests.py
@@ -9,10 +9,12 @@
 # Arezqui Belaid <info@star2billing.com>
 #
 import datetime
+from collections import OrderedDict
 
 from admin_tools_stats.models import DashboardStats, DashboardStatsCriteria
 from admin_tools_stats.utils import BaseAuthenticatedClient, assertContainsAny
 
+from django.core.cache import cache
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 from django.test.utils import override_settings
@@ -87,7 +89,15 @@ class AdminToolsStatsAdminCharts(BaseAuthenticatedClient):
         )
 
 
-class ModelTests(TestCase):
+class ClearCacheMixin(object):
+    def tearDown(self):
+        super().tearDown()
+        cache.clear()
+
+
+class ModelTests(ClearCacheMixin, TestCase):
+    maxDiff=None
+
     def setUp(self):
         self.stats = mommy.make(
             'DashboardStats',
@@ -99,7 +109,7 @@ class ModelTests(TestCase):
 
     @override_settings(USE_TZ=False)
     def test_get_multi_series(self):
-        """Test function to check dashboardstatscriteria admin pages"""
+        """Test function to check DashboardStats.get_multi_time_series()"""
         mommy.make('User', date_joined=datetime.date(2010, 10, 10))
         time_since = datetime.date(2010, 10, 8)
         time_until = datetime.date(2010, 10, 12)
@@ -112,6 +122,209 @@ class ModelTests(TestCase):
             datetime.datetime(2010, 10, 10, 0, 0): {'': 1},
             datetime.datetime(2010, 10, 11, 0, 0): {'': 0},
             datetime.datetime(2010, 10, 12, 0, 0): {'': 0},
+        }
+        self.assertDictEqual(serie, testing_data)
+
+    @override_settings(USE_TZ=False)
+    def test_get_multi_series_dynamic_field_name(self):
+        """Test function to check DashboardStats.get_multi_time_series() with dynamic criteria mapping"""
+        criteria = mommy.make(
+            'DashboardStatsCriteria',
+            criteria_name="active",
+            dynamic_criteria_field_name="is_active",
+            criteria_dynamic_mapping={
+                "": [None, "All"],
+                "false": [False, "Inactive"],
+                "true": [True, "Active"]
+            },
+            use_as='multiple_series',
+        )
+        self.stats.criteria.add(criteria)
+        self.stats.save()
+        mommy.make('User', date_joined=datetime.date(2010, 10, 12), is_active=True)
+        mommy.make('User', date_joined=datetime.date(2010, 10, 13), is_active=False)
+        time_since = datetime.date(2010, 10, 10)
+        time_until = datetime.date(2010, 10, 14)
+
+        interval = "days"
+        serie = self.stats.get_multi_time_series({'select_box_multiple_series': criteria.id}, time_since, time_until, interval)
+        testing_data = {
+            datetime.datetime(2010, 10, 10, 0, 0): OrderedDict((('Active', 0), ('Inactive', 0))),
+            datetime.datetime(2010, 10, 11, 0, 0): OrderedDict((('Active', 0), ('Inactive', 0))),
+            datetime.datetime(2010, 10, 12, 0, 0): OrderedDict((('Active', 1), ('Inactive', 0))),
+            datetime.datetime(2010, 10, 13, 0, 0): OrderedDict((('Active', 0), ('Inactive', 1))),
+            datetime.datetime(2010, 10, 14, 0, 0): OrderedDict((('Active', 0), ('Inactive', 0))),
+        }
+        self.assertDictEqual(serie, testing_data)
+
+    @override_settings(USE_TZ=False)
+    def test_get_multi_series_dynamic_field_name_old_format(self):
+        """
+        Test function to check DashboardStats.get_multi_time_series() with dynamic criteria mapping
+        the criteria is given in old format
+        """
+        criteria = mommy.make(
+            'DashboardStatsCriteria',
+            criteria_name="active",
+            dynamic_criteria_field_name="is_active",
+            criteria_dynamic_mapping={
+                "False": "Inactive",
+                "True": "Active"
+            },
+            use_as='multiple_series',
+        )
+        self.stats.criteria.add(criteria)
+        self.stats.save()
+        mommy.make('User', date_joined=datetime.date(2010, 10, 12), is_active=True)
+        mommy.make('User', date_joined=datetime.date(2010, 10, 13), is_active=False)
+        time_since = datetime.date(2010, 10, 10)
+        time_until = datetime.date(2010, 10, 14)
+
+        interval = "days"
+        serie = self.stats.get_multi_time_series({'select_box_multiple_series': criteria.id}, time_since, time_until, interval)
+        testing_data = {
+            datetime.datetime(2010, 10, 10, 0, 0): OrderedDict((('Active', 0), ('Inactive', 0))),
+            datetime.datetime(2010, 10, 11, 0, 0): OrderedDict((('Active', 0), ('Inactive', 0))),
+            datetime.datetime(2010, 10, 12, 0, 0): OrderedDict((('Active', 1), ('Inactive', 0))),
+            datetime.datetime(2010, 10, 13, 0, 0): OrderedDict((('Active', 0), ('Inactive', 1))),
+            datetime.datetime(2010, 10, 14, 0, 0): OrderedDict((('Active', 0), ('Inactive', 0))),
+        }
+        self.assertDictEqual(serie, testing_data)
+
+    @override_settings(USE_TZ=False)
+    def test_get_multi_series_criteria_without_dynamic_mapping(self):
+        """
+        Test function to check DashboardStats.get_multi_time_series()
+        DashboardStatsCriteria is set, but without dynamic mapping, so the values are autogenerated.
+        """
+        criteria = mommy.make(
+            'DashboardStatsCriteria',
+            criteria_name="active",
+            dynamic_criteria_field_name="is_active",
+            use_as='multiple_series',
+        )
+        self.stats.criteria.add(criteria)
+        self.stats.save()
+        mommy.make('User', date_joined=datetime.date(2010, 10, 12), is_active=True)
+        mommy.make('User', date_joined=datetime.date(2010, 10, 13), is_active=False)
+        time_since = datetime.date(2010, 10, 10)
+        time_until = datetime.date(2010, 10, 14)
+
+        interval = "days"
+        serie = self.stats.get_multi_time_series({'select_box_multiple_series': criteria.id}, time_since, time_until, interval)
+        testing_data = {
+            datetime.datetime(2010, 10, 10, 0, 0): OrderedDict((('True', 0), ('False', 0))),
+            datetime.datetime(2010, 10, 11, 0, 0): OrderedDict((('True', 0), ('False', 0))),
+            datetime.datetime(2010, 10, 12, 0, 0): OrderedDict((('True', 1), ('False', 0))),
+            datetime.datetime(2010, 10, 13, 0, 0): OrderedDict((('True', 0), ('False', 1))),
+            datetime.datetime(2010, 10, 14, 0, 0): OrderedDict((('True', 0), ('False', 0))),
+        }
+        self.assertDictEqual(serie, testing_data)
+
+    @override_settings(USE_TZ=False)
+    def test_get_multi_series_criteria_without_dynamic_mapping_choices(self):
+        """
+        Test function to check DashboardStats.get_multi_time_series()
+        DashboardStatsCriteria is set, but without dynamic mapping, so the values are autogenerated on CharField.
+        """
+        criteria = mommy.make(
+            'DashboardStatsCriteria',
+            criteria_name="name",
+            dynamic_criteria_field_name="last_name",
+            use_as='multiple_series',
+        )
+        self.stats.criteria.add(criteria)
+        self.stats.save()
+        mommy.make('User', date_joined=datetime.date(2010, 10, 12), last_name="Foo")
+        mommy.make('User', date_joined=datetime.date(2010, 10, 13), last_name="Bar")
+        mommy.make('User', date_joined=datetime.date(2010, 10, 14))
+        time_since = datetime.date(2010, 10, 10)
+        time_until = datetime.date(2010, 10, 14)
+
+        interval = "days"
+        serie = self.stats.get_multi_time_series({'select_box_multiple_series': criteria.id}, time_since, time_until, interval)
+        testing_data = {
+            datetime.datetime(2010, 10, 10, 0, 0): OrderedDict((('Bar', 0), ('Foo', 0))),
+            datetime.datetime(2010, 10, 11, 0, 0): OrderedDict((('Bar', 0), ('Foo', 0))),
+            datetime.datetime(2010, 10, 12, 0, 0): OrderedDict((('Bar', 0), ('Foo', 1))),
+            datetime.datetime(2010, 10, 13, 0, 0): OrderedDict((('Bar', 1), ('Foo', 0))),
+            datetime.datetime(2010, 10, 14, 0, 0): OrderedDict((('Bar', 0), ('Foo', 0))),
+        }
+        self.assertDictEqual(serie, testing_data)
+
+    @override_settings(USE_TZ=False)
+    def test_get_multi_series_criteria_combine(self):
+        """
+        Test function to check DashboardStats.get_multi_time_series()
+        Try to combine multiple_series filter with chart_filter.
+        """
+        criteria = mommy.make(
+            'DashboardStatsCriteria',
+            criteria_name="name",
+            dynamic_criteria_field_name="last_name",
+            use_as='multiple_series',
+        )
+        criteria_active = mommy.make(
+            'DashboardStatsCriteria',
+            criteria_name="active",
+            dynamic_criteria_field_name="is_active",
+            use_as='chart_filter',
+        )
+        self.stats.criteria.add(criteria)
+        self.stats.criteria.add(criteria_active)
+        self.stats.save()
+        mommy.make('User', date_joined=datetime.date(2010, 10, 12), last_name="Foo", is_active=True)
+        mommy.make('User', date_joined=datetime.date(2010, 10, 13), last_name="Bar", is_active=False)
+        time_since = datetime.date(2010, 10, 10)
+        time_until = datetime.date(2010, 10, 14)
+
+        interval = "days"
+        arguments = {'select_box_multiple_series': criteria.id, 'select_box_dynamic_%s' % criteria_active.id: 'True'}
+        serie = self.stats.get_multi_time_series(arguments, time_since, time_until, interval)
+        testing_data = {
+            datetime.datetime(2010, 10, 10, 0, 0): OrderedDict((('Bar', 0), ('Foo', 0))),
+            datetime.datetime(2010, 10, 11, 0, 0): OrderedDict((('Bar', 0), ('Foo', 0))),
+            datetime.datetime(2010, 10, 12, 0, 0): OrderedDict((('Bar', 0), ('Foo', 1))),
+            datetime.datetime(2010, 10, 13, 0, 0): OrderedDict((('Bar', 0), ('Foo', 0))),
+            datetime.datetime(2010, 10, 14, 0, 0): OrderedDict((('Bar', 0), ('Foo', 0))),
+        }
+        self.assertDictEqual(serie, testing_data)
+
+    @override_settings(USE_TZ=False)
+    def test_get_multi_series_fixed_criteria(self):
+        """
+        Test function to check DashboardStats.get_multi_time_series()
+        Try to combine multiple_series filter with chart_filter.
+        """
+        criteria = mommy.make(
+            'DashboardStatsCriteria',
+            criteria_name="name",
+            dynamic_criteria_field_name="last_name",
+            use_as='multiple_series',
+        )
+        criteria_active = mommy.make(
+            'DashboardStatsCriteria',
+            criteria_name="active",
+            criteria_fix_mapping={"is_active": True},
+            use_as='chart_filter',
+        )
+        self.stats.criteria.add(criteria)
+        self.stats.criteria.add(criteria_active)
+        self.stats.save()
+        mommy.make('User', date_joined=datetime.date(2010, 10, 12), last_name="Foo", is_active=True)
+        mommy.make('User', date_joined=datetime.date(2010, 10, 13), last_name="Bar", is_active=False)
+        time_since = datetime.date(2010, 10, 10)
+        time_until = datetime.date(2010, 10, 14)
+
+        interval = "days"
+        arguments = {'select_box_multiple_series': criteria.id}
+        serie = self.stats.get_multi_time_series(arguments, time_since, time_until, interval)
+        testing_data = {
+            datetime.datetime(2010, 10, 10, 0, 0): OrderedDict((('Bar', 0), ('Foo', 0))),
+            datetime.datetime(2010, 10, 11, 0, 0): OrderedDict((('Bar', 0), ('Foo', 0))),
+            datetime.datetime(2010, 10, 12, 0, 0): OrderedDict((('Bar', 0), ('Foo', 1))),
+            datetime.datetime(2010, 10, 13, 0, 0): OrderedDict((('Bar', 0), ('Foo', 0))),
+            datetime.datetime(2010, 10, 14, 0, 0): OrderedDict((('Bar', 0), ('Foo', 0))),
         }
         self.assertDictEqual(serie, testing_data)
 
