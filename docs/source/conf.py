b345609e3c8c710d3c267111ef44666ef5036e86
Petr Dlouhý <petr.dlouhy@email.cz>
2020-09-11 14:18:20 +0200

    Bump version: 0.20.2 → 0.21.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ copyright = u'2011-2014, Arezqui Belaid'
 # built documents.
 #
 # The short X.Y version.
-version = '0.20.2'
+version = '0.21.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
