3406e5fca1dd020c2bc96342308d8b9be66e3bb0
Hanno Särg <hannosarg@outlook.com>
2020-09-17 13:55:52 +0300

    More date based stuff

--- admin_tools_stats/models.py
+++ admin_tools_stats/models.py
@@ -346,7 +346,7 @@ class DashboardStats(models.Model):
                         single_value = True
 
                     for dynamic_value in dynamic_values:
-                        criteria_value = m2m.get_dynamic_choices()[dynamic_value]
+                        criteria_value = m2m.get_dynamic_choices(time_since, time_until)[dynamic_value]
                         if isinstance(criteria_value, (list, tuple)):
                             criteria_value = criteria_value[0]
                         else:
@@ -414,7 +414,7 @@ class DashboardStats(models.Model):
         all_criteria = self.criteriatostatsm2m_set.all()  # Outside of get_time_series just for performance reasons
         m2m = self.get_multi_series_criteria(configuration)
         if m2m and m2m.criteria.dynamic_criteria_field_name:
-            choices = m2m.get_dynamic_choices()
+            choices = m2m.get_dynamic_choices(time_since, time_until)
 
             serie_map = {}
             names = []
